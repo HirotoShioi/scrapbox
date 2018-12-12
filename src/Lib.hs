aa3af9f86a30e71c8ae5fdbb1a4651555cc5a10c
Hiroto Shioi <hiroto.shioi@iohk.io>
2018-12-12 12:45:04 +0900

    Overhaul, again

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,54 +11,64 @@ import qualified Data.ByteString.Char8 as C8
 import           Types
 
 -- | Produce ScrapBox Markdown in human readable format
+-- Not working well
 encodePretty :: Markdown -> ByteString
-encodePretty = C8.unlines . encode
+encodePretty = C8.unlines . encodeMarkdown
 
-encode :: Markdown -> [ByteString]
-encode md =
-    let blocks =  getContent md
-    in concatMap (map encodeUtf8 . encodeMarkdown) blocks
+-- | Encode given `Markdown' into list of ByteStrings
+encodeMarkdown :: Markdown -> [ByteString]
+encodeMarkdown md =
+    let blocks =  getMarkdown md
+    in concatMap (map encodeUtf8 . encodeBlocks) blocks
 
-encodeMarkdown :: Block -> [Text]
-encodeMarkdown = \case
-    BreakLine                      -> [""]
-    BlockQuote text                -> [">" <> encodeText text]
-    BulletPoints contents          -> encodeBulletPoints contents
-    BulletLine text                -> ["\t" <> encodeText text]
-    CodeBlock codeName code        -> encodeCodeBlock codeName code
-    Header headerSize text         -> [encodeHeader headerSize (encodeText text)]
-    Simple text                    -> [encodeText text]
-    Table table                    -> encodeTable table
-    Thumbnail (Url url)            -> [block url]
+-- Encode blocks
+encodeBlocks :: Block -> [Text]
+encodeBlocks = \case
+    BreakLine               -> [""]
+    BlockQuote text         -> [">" <> encodeText text]
+    BulletPoints contents   -> encodeBulletPoints contents
+    BulletLine text         -> ["\t" <> encodeText text]
+    CodeBlock codeName code -> encodeCodeBlock codeName code
+    Header headerSize ctx   -> [encodeHeader headerSize ctx]
+    Simple text             -> [encodeText text]
+    Table table             -> encodeTable table
+    Thumbnail (Url url)     -> [blocked url]
 
+-- | Encode given 'ScrapText' into text
 encodeText :: ScrapText -> Text
 encodeText (ScrapText scraps) = foldr (\scrap acc-> encodeScrapText scrap <> acc) mempty scraps
 
+-- | Encode given 'Scrap' into text
 encodeScrapText :: Scrap -> Text
-encodeScrapText (Scrap style scrap) =
-    let encodedScrap = case scrap of
-            CodeNotation code              -> "`" <> code <> "`"
-            Link (Just linkName) (Url url) -> block $ linkName <> " " <> url
-            Link Nothing (Url url)         -> block url
-            PlainText text                 -> text
-    in encodeWithStyle style encodedScrap
+encodeScrapText (Scrap style ctx) = encodeWithStyle style ctx
 
-encodeWithStyle :: Style -> Text -> Text
-encodeWithStyle style text = case style of
-    None -> text
+encodeContext :: Context -> Text
+encodeContext ctxs = foldr (\ctx acc -> encodeScrapContext ctx <> acc) mempty ctxs
+  where
+    encodeScrapContext :: ScrapContext -> Text
+    encodeScrapContext = \case
+        CodeNotation code              -> "`" <> code <> "`"
+        Link (Just linkName) (Url url) -> blocked $ linkName <> " " <> url
+        Link Nothing (Url url)         -> blocked url
+        PlainText text                 -> text
+
+-- | Encode with style (Do not export this)
+encodeWithStyle :: Style -> Context -> Text
+encodeWithStyle style ctx = case style of
+    None -> encodeContext ctx
     Bold -> 
         let boldStyle = StyleData 0 True False False
-        in encodeStyled boldStyle text
+        in encodeStyled boldStyle ctx
     Italic -> 
         let italicStyle = StyleData 0 False True False
-        in encodeStyled italicStyle text
+        in encodeStyled italicStyle ctx
     StrikeThrough ->
         let strikeThroughStyle = StyleData 0 False False True
-        in encodeStyled strikeThroughStyle text
-    Custom style' -> encodeStyled style' text
+        in encodeStyled strikeThroughStyle ctx
+    Custom style' -> encodeStyled style' ctx
 
-encodeStyled :: StyleData -> Text -> Text
-encodeStyled (StyleData headerNum isBold isItalic isStrikeThrough) text =
+encodeStyled :: StyleData -> Context -> Text
+encodeStyled (StyleData headerNum isBold isItalic isStrikeThrough) ctx =
     let italic         = if isItalic then "/" else ""
         strikeThrough  = if isStrikeThrough then "-" else ""
         bold           = if isBold then "*" else ""
@@ -70,37 +80,29 @@ encodeStyled (StyleData headerNum isBold isItalic isStrikeThrough) text =
             , strikeThrough
             , " "
             ]
-    in block $ combinedSyntax <> text
+    in blocked $ combinedSyntax <> (encodeContext ctx)
 
-encodeCodeBlock :: CodeName -> Text -> [Text]
+-- | Encode 'CodeBlock'
+encodeCodeBlock :: CodeName -> CodeSnippet -> [Text]
 encodeCodeBlock (CodeName name) code = do
     let codeName = "code:" <> name
-    case headMaybe (T.lines code) of
-        Nothing -> [codeName]
-        Just codeHead -> do
-            let codeHead' = "\t" <> codeHead
-                rest      = snd $ splitAt 1 (T.lines code)
-                codeRest = map (\codeLine -> " " <> codeLine) rest
-            [codeName] <> [codeHead'] <> codeRest
+    let codeContent = map (\line -> " " <> line) (T.lines $ getCodeSnippet code)
+    [codeName] <> codeContent
 
+-- | Encode an table
 encodeTable :: TableContent -> [Text]
 encodeTable tableContent = undefined
 
+-- | Encode bulletpoints
 encodeBulletPoints :: [ScrapText] -> [Text]
 encodeBulletPoints text = map (\scrapText -> "\t" <> encodeText scrapText) text
 
-encodeHeader :: Int -> Text -> Text
-encodeHeader headerSize scrapText = 
+-- | Encode header
+encodeHeader :: Int -> Context -> Text
+encodeHeader headerSize ctx = 
     let style     = StyleData headerSize False False False
-    in encodeStyled style scrapText
+    in encodeStyled style ctx
 
-block :: Text -> Text
-block content = "[" <> content <> "]"
-
--- decodeHtml :: ???? -> Parser Content
--- decodeHtml = undefined
-
--- decodeMarkdown :: ???? -> Parser Content
--- decodeMarkdown = undefined
-
--- Need to make an chart
+-- | Add an block to a given encoded text
+blocked :: Text -> Text
+blocked content = "[" <> content <> "]"
--- src/Types.hs
+++ src/Types.hs
@@ -5,6 +5,8 @@ import RIO.Time
 
 -- https://scrapbox.io/help/Syntax
 
+-- TODO: Make naming more easy to understand/ makes sense
+
 data Page = Page
     { pContent   :: !Markdown
     , pCreatedAt :: !UTCTime
@@ -12,47 +14,7 @@ data Page = Page
     , pTitle     :: !Text
     } deriving (Eq, Show)
 
-newtype Markdown = Markdown 
-    { getContent :: [Block]
-    } deriving (Eq, Show)
-
-data Block
-    = BreakLine
-    | BlockQuote ScrapText
-    | BulletLine ScrapText
-    | BulletPoints [ScrapText]
-    | CodeBlock CodeName Code
-    | Header Int ScrapText
-    | Simple ScrapText
-    | Table TableContent -- No sure how to implement yet!!
-    | Thumbnail Url
-    deriving (Eq, Show)
-
-newtype ScrapText = ScrapText {
-    getScrapText :: [Scrap] 
-    } deriving (Eq, Show)
-
-data Scrap = Scrap
-    { sStyle :: Style
-    , sContent :: Context
-    } deriving (Eq, Show)
-
-data Style = 
-      Custom StyleData
-    | Bold
-    | Italic
-    | None
-    | StrikeThrough
-    deriving (Eq, Show)
-
-data Context =
-      CodeNotation Text
-    | Link (Maybe Text) Url
-    | PlainText Text
-    deriving (Eq, Show)
-
 type TableContent = [[Text]]
-type Code = Text
 
 newtype Url = Url 
     { getUrl :: Text
@@ -62,9 +24,81 @@ newtype CodeName = CodeName
     { getCodeName :: Text
     } deriving (Eq, Show)
 
+newtype CodeSnippet = CodeSnippet
+    { getCodeSnippet :: Text
+    } deriving (Eq, Show)
+
+-- | Markdown consist of list of 'Blocks'
+newtype Markdown = Markdown 
+    { getMarkdown :: [Block]
+    } deriving (Eq, Show)
+
+-- | Blocks are contents
+data Block
+    = BreakLine
+    -- ^ Simply breaks a line
+    | BlockQuote ScrapText
+    -- ^ BlockQuote like markdown
+    | BulletLine ScrapText
+    -- ^ Bulletpoint styled line
+    | BulletPoints [ScrapText]
+    -- ^ Bullet points
+    | CodeBlock CodeName CodeSnippet
+    -- ^ Code blocks
+    | Header Int Context
+    -- ^ Header
+    | Simple ScrapText
+    -- ^ Simple text
+    | Table TableContent -- No sure how to implement yet!!
+    -- ^ Table
+    | Thumbnail Url
+    -- ^ Thumbnail
+    deriving (Eq, Show)
+
+-- | ScrapText are consisted by list of Scrap which are context associated with an style
+newtype ScrapText = ScrapText {
+    getScrapText :: [Scrap] 
+    } deriving (Eq, Show)
+
+-- | Scrap is an context which can have a style
+data Scrap = Scrap
+    { sStyle   :: !Style
+    , sContent :: !Context
+    } deriving (Eq, Show)
+
+type Context = [ScrapContext]
+
+-- | Style that can be applied to the 'Context'
+data Style = 
+      Custom StyleData
+    -- ^ You can use this to combine all three as of the styles as well as Header
+    | Bold
+    -- ^ Bold style
+    | Italic
+    -- ^ Italic style
+    | None
+    -- ^ None
+    | StrikeThrough
+    deriving (Eq, Show)
+
+-- | Context
+data ScrapContext =
+      CodeNotation Text
+    -- ^ CodeNotation
+    | Link (Maybe Text) Url
+    -- ^ Link, it can have href
+    | PlainText Text
+    -- ^ Just an simple text
+    deriving (Eq, Show)
+
+-- | StyleData
 data StyleData = StyleData
     { bHeader        :: !Int
+    -- ^ Size of an header,
     , bBold          :: !Bool
+    -- ^ Bold style
     , bItalic        :: !Bool
+    -- ^ Italic style
     , bStrikeThrough :: !Bool
+    -- ^ Strike through
     } deriving (Eq, Show)
