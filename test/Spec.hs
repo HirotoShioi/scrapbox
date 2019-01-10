80a1b1efb2fa293c14b67bc45a8841b98faa7b42
Hiroto Shioi <hiroto.shioi@iohk.io>
2019-01-10 11:19:22 +0900

    Explicit import

--- test/Spec.hs
+++ test/Spec.hs
@@ -11,7 +11,7 @@ import           Test.Hspec.QuickCheck   (modifyMaxSuccess)
 
 import           TestCommonMark.Segments (segmentSpec)
 import           TestCommonMark.Styles   (styleSpec)
-import           TestCommonMark.Blocks
+import           TestCommonMark.Blocks   (blockSpec)
 
 main :: IO ()
 main = hspec $ do
